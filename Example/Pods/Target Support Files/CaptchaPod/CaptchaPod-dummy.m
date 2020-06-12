710472c82c59ffb512c656140c89b8ab3edc782e
Amr Hesham <amr.hesham@fawry.com>
2020-06-12 18:16:53 +0200

    initial commit with pod lib template

---
+++ Example/Pods/Target Support Files/CaptchaPod/CaptchaPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CaptchaPod : NSObject
+@end
+@implementation PodsDummy_CaptchaPod
+@end
---
+++ Example/Pods/Target Support Files/Pods-CaptchaPod_Tests/Pods-CaptchaPod_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CaptchaPod_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_CaptchaPod_Tests
+@end
